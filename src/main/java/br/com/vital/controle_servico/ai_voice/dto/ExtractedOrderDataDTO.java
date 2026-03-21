de6102c339b758056b82abe85000638fc3272317
welbber porto <welbervital01@gmail.com>
2026-03-21 15:16:16 -0300

    Ajustes no modulo ai voice montarmos uma lista completa dos itens

--- src/main/java/br/com/vital/controle_servico/ai_voice/dto/ExtractedOrderDataDTO.java
+++ src/main/java/br/com/vital/controle_servico/ai_voice/dto/ExtractedOrderDataDTO.java
@@ -1,10 +1,16 @@
 package br.com.vital.controle_servico.ai_voice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
-public record ExtractedOrderDataDTO(
-        String customerComplaint,
-        List<String> suggestedParts,
-        List<String> suggestedServices
-) {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExtractedOrderDataDTO {
+    private String customerComplaint;
+    private List<SuggestedItemDTO> suggestedParts;
+    private List<SuggestedItemDTO> suggestedServices;
 }
